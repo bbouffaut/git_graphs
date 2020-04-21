6a5f32ec597d6a996ee7a2bb0fb837c630ec4c1a
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2020-04-21 23:57:04 +0200

    CovidIA git feature branch strategy

--- covidia/covidia-1.js
+++ covidia/covidia-1.js
@@ -2,9 +2,11 @@ const graphContainer = document.getElementById("graph-container");
  
 // Display options
 var withoutAuthor = GitgraphJS.templateExtend("metro", {
+  colors: ["#979797", "#008fb5", "#f1c109", "#9966ff", "#3399ff"],
   commit: {
     message: {
       displayAuthor: false,
+      displayHash: false,
     },
   },
 });
@@ -20,16 +22,29 @@ const gitgraph = GitgraphJS.createGitgraph(graphContainer,options);
 const master = gitgraph.branch("master");
 master.commit("Initial commit");
 
-const develop = gitgraph.branch("develop");
-develop.commit("Add TypeScript");
+const branch1 = gitgraph.branch("E2-US1");
+const branch2 = gitgraph.branch("E2-US2");
+branch1.commit("Implement Health data retrieval");
+branch2.commit("Implement Demographic data retrieval");
+const branch3 = gitgraph.branch({
+  name: "E2-US2-user1",
+  from: branch2,
+});
+const branch4 = gitgraph.branch({
+  name: "E2-US2-user2",
+  from: branch2,
+});
+branch3.commit("Implement per department display");
+branch4.commit("Implement time-line");
+branch2.merge(branch4);
+branch3.merge(branch2);
+branch1.commit("Expose Health data with API");
+branch3.commit("Integrate with FE");
+branch2.merge(branch3);
+branch2.commit("Expose Demographic data with API");
 
-const aFeature = gitgraph.branch("a-feature");
-aFeature
-  .commit("Make it work")
-  .commit({ subject: "Make it right", hash: "test" })
-  .commit("Make it fast");
+master.merge(branch2);
+branch1.merge(master);
 
-develop.merge(aFeature);
-develop.commit("Prepare v1");
-
-master.merge(develop).tag("v1.0.0");
+branch1.commit("Integrate API with App BE");
+master.merge(branch1).tag("Production");
