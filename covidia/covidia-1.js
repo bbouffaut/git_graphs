f8e9772bf9b4c95de891912deb7aa8206d48f8a5
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2020-04-21 21:52:09 +0200

    Clone demo for Covidia-: play with some options

---
+++ covidia/covidia-1.js
@@ -0,0 +1,35 @@
+const graphContainer = document.getElementById("graph-container");
+ 
+// Display options
+var withoutAuthor = GitgraphJS.templateExtend("metro", {
+  commit: {
+    message: {
+      displayAuthor: false,
+    },
+  },
+});
+const options = {
+  template: withoutAuthor,
+  orientation: 'vertical-reverse'
+}
+
+// Instantiate the graph.
+const gitgraph = GitgraphJS.createGitgraph(graphContainer,options);
+
+// Simulate git commands with Gitgraph API.
+const master = gitgraph.branch("master");
+master.commit("Initial commit");
+
+const develop = gitgraph.branch("develop");
+develop.commit("Add TypeScript");
+
+const aFeature = gitgraph.branch("a-feature");
+aFeature
+  .commit("Make it work")
+  .commit({ subject: "Make it right", hash: "test" })
+  .commit("Make it fast");
+
+develop.merge(aFeature);
+develop.commit("Prepare v1");
+
+master.merge(develop).tag("v1.0.0");
